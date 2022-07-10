3d7d42aa38437e4ddc9b4c1f6975397294b2dcf8
Daseull <59015764+Daseull@users.noreply.github.com>
2022-07-11 06:31:08 +0900

    [Gold II] Title: 벡터 매칭, Time: 132 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/1007. 벡터 매칭/벡터 매칭.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int N;
+vector<pair<int, int>> points;
+#define MAX 200000000
+
+int count(int visited) {
+	int ret = 0;
+	while (visited > 0) {
+		ret += visited % 2;
+		visited /= 2;
+	}
+	return ret;
+}
+
+double min(double a, double b) {
+	return a < b ? a : b;
+}
+
+double solution(int idx, int s, int e, int nMatching, long long x, long long y) {
+	if (idx == N) {
+		if (nMatching == N && s == N/2) {
+			return sqrt(x * x + y * y);
+		}
+		return MAX;
+	}
+	double ret = MAX;
+	ret = min(ret, solution(idx+1, s+1, e, nMatching + 1, x - points[idx].first, y - points[idx].second));
+	ret = min(ret, solution(idx+1, s, e+1, nMatching + 1, x + points[idx].first, y + points[idx].second));
+	return ret;
+}
+
+
+
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int t;
+	cin >> t;
+	while (t--) {
+		cin >> N;
+		pair<int, int> p;
+		for (int i = 0; i < N; i++) {
+			cin >> p.first >> p.second;
+			points.push_back(p);
+		}
+		cout.precision(15);
+		cout << solution(0, 0,0,0,0,0) << "\n";
+		points.clear();
+	}
+}
