11eac74bd141505026aa4a3e7d16d0727f386dd4
Daseull <59015764+Daseull@users.noreply.github.com>
2022-07-24 21:04:28 +0900

    [Gold V] Title: 로봇 프로젝트, Time: 3804 ms, Memory: 5936 KB -BaekjoonHub

---
+++ 백준/Gold/3649. 로봇 프로젝트/로봇 프로젝트.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void solution(vector<int> &lego, int x, int n) {
+	int start = 0, end = n - 1;
+	while (start != end) {
+		if (lego[start] + lego[end] == x) {
+			cout << "yes " << lego[start] << " " << lego[end] << "\n";
+			return;
+		}
+
+		if (lego[start] + lego[end] < x)
+			start++;
+		else
+			end--;
+	}
+	cout << "danger\n";
+}
+
+
+int main() {
+	int x, n;
+	while (cin >> x >> n) {
+		x *= 10000000;
+		vector<int> lego(n);
+		for (int i = 0; i < n; i++)
+			cin >> lego[i];
+
+		if (n < 2) {
+			cout << "danger\n";
+			continue;
+		}
+
+		sort(lego.begin(), lego.end());
+		solution(lego, x, n);
+	}
+}
